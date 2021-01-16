2476a4e430dbc90c2afcfd654de9da3e53d577bd
ErenyYousri <76994119+ErenyYousri@users.noreply.github.com>
2021-01-16 08:32:08 -0800

    Add files via upload

---
+++ server2.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const hbs = require('hbs')
+const express = require('express')
+const apidata = require('./utlils/apiData')
+const app = express()
+
+const publicDirectory = path.join(__dirname, '../public')
+const viewsDirectory = path.join(__dirname, '../templet2/views2')
+const partialsDirectory = path.join(__dirname, '../templet2/partials2')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsDirectory)
+hbs.registerPartials(partialsDirectory)
+
+app.use(express.static(publicDirectory))
+
+app.get('', (req, res)=>{
+    apidata.getAllData( (err, apiResponse)=>{
+        if(!err){
+            flag = false
+            mydata = apiResponse
+        }
+        else{
+            flag=true
+            mydata = []
+        }
+        res.render('index', {mydata})
+    })
+} )
+
+
+app.get('/id', (req, res)=>{
+    apidata.getSingle(1, (err, apiResponse)=>{
+        if(!err){
+            flag = false
+            mydata = apiResponse
+        }
+        else{
+            flag=true
+            mydata = []
+        }
+        console.log(mydata)
+        res.render('single', {mydata})
+    })
+} )
+
+app.listen(3000)
+
+
+
